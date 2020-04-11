b0491cd029e9c6dc7a415e06e990c116de2bb679
hugo <hugo.bilquart@gmail.com>
2020-04-11 17:42:14 +0200

    Sqlite + taille des oeufs

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-var fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+const date = require('date-and-time');
 
+var fs = require('fs');
 var express = require('express')
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server)
+
+function createDb() {
+    let db = new sqlite3.Database('ranking/classement.sqlite');
+            
+    db.run('CREATE TABLE `classement` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `date` TEXT NOT NULL, `pseudo` TEXT, `time` TEXT NOT NULL )');
+    
+    db.close();
+}
+
+function getRanking() {
+    let db = new sqlite3.Database('ranking/classement.sqlite');
+
+    var classement = new Array();
+
+    db.serialize(function() {
+        db.each("SELECT * FROM classement ORDER BY time", (error, row) => {
+            classement.push(row);
+        });
+    });
+
+    db.close();
+
+    return classement;
+}
 
 //Dossier ressources (images, css, js)
 app.use('/', express.static('ressources'));
@@ -28,6 +54,24 @@ io.sockets.on('connection', function (socket) {
             socket.emit('finished');
             console.log('Partie terminée');
         }
+    });
+
+    socket.on('pseudo', function(pseudo,chrono) {
+        let db = new sqlite3.Database('ranking/classement.sqlite');
+
+        var now = new Date();
+        var currentdate = date.format(now, 'YYYY-MM-DD HH:mm:ss');
+
+        db.run('INSERT INTO classement(date, pseudo, time) VALUES(?, ?, ?)', [currentdate,pseudo,chrono], (err) => {
+            if(err) {
+                return console.log(err.message); 
+            }
+            console.log('Score enregistré');
+        });
+
+        db.close();
+
+        socket.emit('ranked');
     });
 });
 
